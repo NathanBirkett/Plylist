effff4fe9b03424b375349486725a35db0c879e6
PrinterPaper101 <nathan.birkett@icsd.k12.ny.us>
2022-04-09 07:27:20 -0400

    functioning playlist and started on randomization algorithm

--- main.py
+++ main.py
@@ -24,16 +24,56 @@ def install_file(url, name, playlist):
 
 
 def play(playlist):
-    songs = os.listdir("playlists/" + playlist)
-    song = songs[random.randrange(len(songs))]
+    song_list = os.listdir("playlists/" + playlist)
+    songs = random.sample(song_list, len(song_list))
+    create_list(songs, playlist)
+    print(songs)
+    play_song(playlist, songs, 0)
+    play(playlist)
+
+
+def create_list(songs, playlist):
+    music_list = random.sample(songs, len(songs))
+    for song in music_list:
+        print(music_length(song, playlist))
+
+"""
+ex: 3 songs: A: 1:30, B: 4:00, C: 1:00
+sample random (A, C, B)
+find longest element (B)
+
+for each element in sample:         (A)     (B)     (C)
+    sum total element time          (1:30)  (4:00)  (1:00)
+    error = longest - el_time       (2:30)  (0)     (3:00)
+    if error < 60000:               (no)    (yes)   (no)
+        nothing
+    else:
+        randomly insert duplicate of element    (A, A, C, C, A, B, C, C)
+repeat for loop
+"""
+
+
+def music_length(song, playlist):
     p = vlc.MediaPlayer("playlists/" + playlist + "/" + song)
+    p.play()
+    time.sleep(1.5)
+    length = p.get_length()
+    p.stop()
+    return length
+
+
+def play_song(playlist, songs, index):
+    if index == len(songs):
+        return
+    p = vlc.MediaPlayer("playlists/" + playlist + "/" + songs[index])
     p.audio_set_volume(50)
     p.play()
-    print("playing: " + song)
+    print("playing: " + songs[index])
     time.sleep(1.5)
     time.sleep(p.get_length() / 1000)
     p.stop()
-    play(playlist)
+    index += 1
+    play_song(playlist, songs, index)
 
 
 def run():
@@ -53,6 +93,3 @@ def run():
 
 
 run()
-# new_playlist(str(input("name: ")))
-# install_file(str(input("url: ")), str(input("name: ")))
-# play_file(str(input("name: ")))
